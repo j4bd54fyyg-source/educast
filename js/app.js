c5d161343dcb52235fc0eb6a245f95054d23b2f8
David <david@M3.local>
2026-06-17 20:36:49 +0200

    Pridaný maturitný test RUJ B2 2010

--- js/app.js
+++ js/app.js
@@ -2420,6 +2420,10 @@ fetch('data/ruj_b1_2010.json')
   .then(function(r){return r.json();})
   .then(function(qs){addMat('ruj-b1',2010,qs);})
   .catch(function(e){console.error('Chyba RUJ B1:',e);});
+fetch('data/ruj_b2_2010.json')
+  .then(function(r){return r.json();})
+  .then(function(qs){addMat('ruj-b2',2010,qs);})
+  .catch(function(e){console.error('Chyba RUJ B2:',e);});
 // addMat('ruj-b2', 2010, [...]);
 // addMat('frj-b1', 2010, [...]);
 // addMat('frj-b2', 2010, [...]);
